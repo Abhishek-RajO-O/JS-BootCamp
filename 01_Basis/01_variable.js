8f85b7b8fa6576457faa441eef4ea0a87492ab48
Abhishek Raj <abhishekraj605912@gmail.com>
2026-06-17 00:00:01 +0530

    day 1 16/6/26

---
+++ 01_Basis/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = "236224"
+let accountEmail = "example@gamil.com"
+var accountPassword = "12345"
+accountCity = "Jsr"
+
+// accountId = 8  // not allowed 
+accountEmail = "example2@gmail.com"
+accountPassword = "989786434"
+accountCity = "bbsr"
+
+let accountState;
+/*
+prefer not to use var 
+beacuse it has no block concept 
+*/
+
+
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_Basis/02_DataType.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3) we are using node js not browser
+
+let start = null 
+
+// null => standalone value 
+// undifined   => value not assigned 
+// symbol => uniqness
+
+// objects 
+
+console.log(typeof start); //object, null is objecrt 
+console.log(typeof undefined); //undifined
+
+
+ 
