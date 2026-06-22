8cffb7ddbdeb214af8b65db3ab0972cc7a4c4ce6
Abhishek Raj <abhishekraj605912@gmail.com>
2026-06-23 01:17:30 +0530

    Project3 Digital Clock

---
+++ dom-project-chaiaurcode/3-DigitalClock/chaiaurcode.js
@@ -0,0 +1,7 @@
+const clock = document.getElementById("clock");
+
+setInterval(()=>{
+let date = new Date();
+// date.toLocaleTimeString();
+    clock.innerHTML = date.toLocaleTimeString();
+},1000);
