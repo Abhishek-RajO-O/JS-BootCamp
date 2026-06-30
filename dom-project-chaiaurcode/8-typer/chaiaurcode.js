50ffc540eeccc34cb5a2a2dfc59124da9b22f45c
Abhishek Raj <abhishekraj605912@gmail.com>
2026-06-30 23:20:56 +0530

    Project 8 Completed

--- dom-project-chaiaurcode/8-typer/chaiaurcode.js
+++ dom-project-chaiaurcode/8-typer/chaiaurcode.js
@@ -3,6 +3,44 @@ const cursor = document.querySelector('.cursor');
 
 const words = ['Love', 'Jhakaas', 'mast', 'dhinchak', 'Weird'];
 
-function type() {}
+let wordIndex = 0;
+let letterIndex = 0 ;
 
-function erase() {}
+const typeDelay  = 200;
+const eraseDelay = 100;
+const newWordDelay = 1500;
+
+
+function type() {
+    setTimeout(()=>{
+        if(letterIndex < words[wordIndex].length){
+            typedTextSpan.textContent += words[wordIndex].charAt(letterIndex);
+            letterIndex++;
+            type();
+        }else{
+            setTimeout(erase,newWordDelay);
+        }
+
+    },typeDelay);
+}
+
+function erase() {
+    setTimeout(()=>{
+        if(letterIndex > 0){
+                typedTextSpan.textContent = words[wordIndex].substring(0,letterIndex-1);
+                letterIndex--;
+                erase();
+        }else{
+            wordIndex = (wordIndex +1)% words.length;
+            letterIndex = 0 ;
+            type();
+        }
+    },eraseDelay);
+}
+
+
+document.addEventListener("DOMContentLoaded", (e)=>{
+    setTimeout(()=>{
+        type();
+    },1000)
+})
