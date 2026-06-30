1bc7ccc774800c13a35a2fd18740fa9f8cb753c1
Abhishek Raj <abhishekraj605912@gmail.com>
2026-06-30 23:28:03 +0530

    Project  10 Completed

--- dom-project-chaiaurcode/10-emoji/chaiaurcode.js
+++ dom-project-chaiaurcode/10-emoji/chaiaurcode.js
@@ -35,3 +35,8 @@ const emojis = [
   '😁',
   '😬',
 ];
+let index = 0;
+btn.addEventListener('mouseover',(e)=>{
+  index = Math.floor(Math.random()*emojis.length);
+  btn.textContent = emojis[index];
+})
