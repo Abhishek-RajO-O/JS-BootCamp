edaad19b0446d50daaad77e0f6fc74539e66faa8
Abhishek Raj <abhishekraj605912@gmail.com>
2026-07-01 15:14:30 +0530

    Project 13 Completed

--- dom-project-chaiaurcode/13-jokes/chaiaurcode.js
+++ dom-project-chaiaurcode/13-jokes/chaiaurcode.js
@@ -1,7 +1,42 @@
-const url = 'https://api.chucknorris.io/jokes/random';
-
+const url = "https://api.chucknorris.io/jokes/random";
+const btn = document.querySelector(".btn");
+const output = document.querySelector("#display-joke");
 // handle this end point with XMLHttpRequest
+// const xhr = new XMLHttpRequest();
+
+// let joke = null;
+
+// btn.addEventListener('click',(e)=>{
+//     // console.log('clicked');
+//     // console.log(xhr.readyState);
+//     xhr.onreadystatechange = function(){
+//         // console.log(xhr.readyState);
+
+//         if(xhr.readyState === 4){
+//             joke = this.responseText;
+//             joke = JSON.parse(joke).value;
+//             // console.log(joke);
+//             output.textContent = `${joke}`;
+//         }
+//     }
+//     xhr.open('GET',url,true);
+//     xhr.send();
+// })
 
 // handle this end point with promises
+btn.addEventListener("click", (e) => {
+  fetch(url)
+    .then((response) => {
+      let data = response.json();
+      return data;
+    })
+    .then((joke) => {
+      joke = joke.value;
+      output.textContent = `${joke}`;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 // handle the case of race condition
